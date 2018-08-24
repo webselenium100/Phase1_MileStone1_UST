117fa64190ad65685cf0420b68ea2c5f3e8b6fa9
raghunath reddy <raghunath reddy@raghunathreddy>
2018-08-24 09:15:26 +0530

    24-08-2018

--- Phase1_MileStone1_JDW/src/com/cname/core/framework/utils/Global.java
+++ Phase1_MileStone1_JDW/src/com/cname/core/framework/utils/Global.java
@@ -3,9 +3,11 @@ package com.cname.core.framework.utils;
 public class Global 
 {
 	public static final String browserName = "chrome";
+	public static final String testDataPath = "D:\\Selenium_resource\\GITLOCALREPO\\Phase1_MileStone1_JDW\\TestDataSuite\\TestData.xlsx";
 	public static final String proPath = "C:\\Users\\raghunath reddy\\eclipse-workspace\\Phase1_MileStone1_JDW\\Environment\\config.properties";
 	public static final String chroPath = "C:\\Users\\raghunath reddy\\eclipse-workspace\\Phase1_MileStone1_JDW\\Server\\chromedriver.exe";
 	public static final String ffPath = "C:\\Users\\raghunath reddy\\eclipse-workspace\\Phase1_MileStone1_JDW\\Server\\geckodriver.exe";
 	public static final String iePath = "C:\\Users\\raghunath reddy\\eclipse-workspace\\Phase1_MileStone1_JDW\\Server\\IEDriverServer.exe";
+	
 	
 }
--- Phase1_MileStone1_JDW/src/com/cname/projectname/testscenarios/appname/Test1.java
+++ Phase1_MileStone1_JDW/src/com/cname/projectname/testscenarios/appname/Test1.java
@@ -3,15 +3,19 @@ package com.cname.projectname.testscenarios.appname;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.cname.core.framework.datalayer.Excel;
 import com.cname.core.framework.datalayer.Property;
+import com.cname.core.framework.sewebdriver.SeWebDriver;
 import com.cname.projectname.testinitialization.Driver;
 
 public class Test1
 {
 	public WebDriver driver;
+	
 	@BeforeClass
 	public void init()
 	{
@@ -21,7 +25,11 @@ public class Test1
 	@Test
 	public void test1()
 	{
-		driver.get(Property.getProValue("url"));
+		String url = Excel.getData("Sheet1", 2, 4);
+		SeWebDriver sdriver = new SeWebDriver();
+		sdriver.passUrl(url);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		WebElement w1 = sdriver.findWebElement("xpath", "//input[@id='searchString1']");
+		w1.sendKeys("Jeans");
 	}
 }
